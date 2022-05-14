fbfc751283c3719ad747f80bbdb1c2b50dd8f931
tr1ssys <aleksanderrr1@gmail.com>
2022-05-14 23:32:45 +0300

    v2

--- src/FourthPractice/main.java
+++ src/FourthPractice/main.java
@@ -4,5 +4,9 @@ public class main {
     public static void main(String[] args) {
     Person terry = new Person(45,"Terry Jack Davidson");
     Person average = new Person();
+    terry.move();
+    terry.talk();
+    average.move();
+    average.talk();
     }
 }
